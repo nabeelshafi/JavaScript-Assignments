c343a5ed3a8c1e6d32478c1b7d0bc13f805a93e8
nabeelshafi <nabeelshafi.ncm@outlook.com>
2024-04-23 11:44:31 +0500

    fix bugs & add hover effect

--- Calculator/app.js
+++ Calculator/app.js
@@ -1,39 +1,39 @@
 var inputField = document.getElementById("inputField");
 
-function inputVal(val){
+function inputVal(val) {
   inputField.value += val;
 }
 
-function equSol(){
-  try{
-    if(inputField.value){
-    inputField.value = eval(inputField.value);
+function equSol() {
+  try {
+    if (inputField.value) {
+      inputField.value = eval(inputField.value);
     }
-      
+
   }
-  catch(err){
+  catch (err) {
     alert("Invalid Input");
   }
 }
 
 
-function clr(){
+function clr() {
   inputField.value = inputField.value.slice(0, -1);;
 }
 
-function allClear(){
+function allClear() {
   inputField.value = "";
 }
 
 var tempLength = 0;
 
-function perSol(){
-  if(inputField.value){
-  var floatVal = parseFloat(inputField.value)
-  var secVal = inputField.value.split("-").slice(-1)[0]
-  var result = floatVal / 100 * secVal;
-  tempLength = floatVal.toString().length + 1;
-  inputField.value = eval(inputField.value.slice(0,tempLength) + result);
+function perSol() {
+  if (inputField.value) {
+    var floatVal = parseFloat(inputField.value)
+    var secVal = inputField.value.split("-").slice(-1)[0]
+    var result = floatVal / 100 * secVal;
+    tempLength = floatVal.toString().length + 1;
+    inputField.value = eval(inputField.value.slice(0, tempLength) + result);
   }
-    
+
 }
